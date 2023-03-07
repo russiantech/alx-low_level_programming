697b908a35530d8fd913772827190b3b17bce013
russiantech <chrisjsmez@gmail.com>
2023-03-06 23:16:22 -0800

    another commit without executables yet

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - a func that copies memory area
+ * @dest: current memory location
+ * @src: memory move/copiedd to
+ * @n: num of bytes
+ *
+ * Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
