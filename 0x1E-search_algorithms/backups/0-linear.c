0e1e1246df5b3495d9f2a5f2b9f99bb7e6f85864
russiantech <chrisjsmez@gmail.com>
2023-12-13 10:00:52 -0800

    betty-compliance & global variables fixed

--- 0x1E-search_algorithms/backups/0-linear.c
+++ 0x1E-search_algorithms/backups/0-linear.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
@@ -11,14 +12,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i; /* Declare i outside the loop */
-
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n",
+		       i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
