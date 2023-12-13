a0ed050ce0d8a9035b2881ba62e26dabc5d0d4f5
russiantech <chrisjsmez@gmail.com>
2023-12-13 08:36:50 -0800

    betty errors fixed on linear-search dot c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,20 +7,21 @@
  * @size: Number of elements in the array
  * @value: Value to search for
  *
- * Return: Index where the value is located, or -1 if not present or if array is NULL
+ * Return: Index where the value is located,
+ * or -1 if not present or if array is NULL
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return (-1);
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+		       i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-    return -1;
+	return (-1);
 }
